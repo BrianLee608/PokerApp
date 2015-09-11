53b00b0e0c5fdd7cd20d7feca941583e64943d43
Dennis Deng <dennisdeng2002@yahoo.com>
2015-09-11 16:37:36 -0400

    Fixed adding board to player cards

--- src/poker/HandEvaluator.java
+++ src/poker/HandEvaluator.java
@@ -30,22 +30,30 @@ public class HandEvaluator {
 	//will be made
 	public static Player evaluateHands(Player[] players, Card[] board) {
 
-		int[][] playerStrengths = new int[players.length][1];
+		int[][] playerStrengths = new int[players.length][5];
 
-		ArrayList <Card> allCards = new ArrayList <Card> ();
+		Card[] allCards = new Card[7];
+		allCards[0] = board[0];
+		allCards[1] = board[1];
+		allCards[2] = board[2];
+		allCards[3] = board[3];
+		allCards[4] = board[4];
+
+
 		for (int i = 0; i < players.length; i++) {
-			
-			allCards.addAll(Arrays.asList(players[i].holeCards[0], players[i].holeCards[1],
-					board[0], board[1], board[2], board[3], board[4]));
-			System.out.println(allCards);
-			sort(allCards);
-			System.out.println(allCards);
 
-			playerStrengths[i] = determineStrength(allCards);
+			allCards[5] = players[i].holeCards[0];
+			allCards[6] = players[i].holeCards[1];
+
+			ArrayList<Card> allCardz = new ArrayList<Card>(Arrays.asList(allCards));
+
+			sort(allCardz);
+			System.out.println(allCardz);
+
+			playerStrengths[i] = determineStrength(allCardz);
 			System.out.println(Arrays.toString(playerStrengths[i]));
 			}
 		
-
 		return players[determineWinner(playerStrengths)];
 
 	}
@@ -62,23 +70,26 @@ public class HandEvaluator {
 
 	//the passed in cards must be sorted
 	public static int[] hasFourOfAKind(ArrayList<Card> cards) {
+		//Store temporary arraylist of cards
+		ArrayList<Card> tempCards = new ArrayList<Card>(cards);
+
 		int quadCounter = 0;
 		int quadValue = 0;
 		int kickerValue = 0;
 		//if you have 4 of a kind, it must be in indicies 0-3 through 3-6
 		outerloop : for (int i = 0; i <= 3; i++) {
 			quadCounter = 0; //reset quadCounter;
-			for (int a = i+1; a < cards.size(); a++) {
-				if (cards.get(i).getRank().equals(cards.get(a).getRank())) {
+			for (int a = i+1; a < tempCards.size(); a++) {
+				if (tempCards.get(i).getRank().equals(tempCards.get(a).getRank())) {
 					quadCounter++;
 					if (quadCounter == 3) { //if you have quads
 						//retVal[1]
-						quadValue = cards.get(i).getRank().getNumeral();
+						quadValue = tempCards.get(i).getRank().getNumeral();
 						//delete all cards that are included in the quad (4 cards)
 						//sublist(inclusive, exclusive)
-						cards.subList(i, i+4).clear();
+						tempCards.subList(i, i+4).clear();
 						//last value in this array is high card
-						kickerValue = cards.get(2).getRank().getNumeral();
+						kickerValue = tempCards.get(2).getRank().getNumeral();
 						break outerloop;
 					}
 				} else {
@@ -101,6 +112,9 @@ public class HandEvaluator {
 }
 
 	public static int[] hasFullHouse(ArrayList <Card> cards) {
+		//Store temporary arraylist of cards
+		ArrayList<Card> tempCards = new ArrayList<Card>(cards);
+
 		int tripsCounter = 0;
 		int pairCounter = 0;
 		//Fullhouse is comprised of trips + pair
@@ -108,14 +122,14 @@ public class HandEvaluator {
 		int pairValue = 0;
 		//Find highest 3 of a kind
 		//Uses same method as quads, but starts from the end (finds highest trips possible)
-		outerloop : for(int i = cards.size()-1; i >= 3; i--){
+		outerloop : for(int i = tempCards.size()-1; i >= 2; i--){
 			tripsCounter = 0;
 			for (int a = i-1; a >= 0; a--){
-				if(cards.get(i).getRank().equals(cards.get(a).getRank())){
+				if(tempCards.get(i).getRank().equals(tempCards.get(a).getRank())){
 					tripsCounter++;
 					if (tripsCounter == 2){
-						tripsValue = cards.get(i).getRank().getNumeral();
-						cards.subList(i - 2, i+1).clear();
+						tripsValue = tempCards.get(i).getRank().getNumeral();
+						tempCards.subList(i - 2, i+1).clear();
 						break outerloop;
 					}
 				} else{
@@ -123,22 +137,26 @@ public class HandEvaluator {
 				}
 			}
 		}
-		//Find highest remaining pair
-		outerloop : for(int i = cards.size()-1; i >= 3; i--) {
-			pairCounter = 0;
-			for (int a = i - 1; a >= 0; a--) {
-				if (cards.get(i).getRank().equals(cards.get(a).getRank())) {
-					pairCounter++;
-					//Must be greater or equal to one (if two trips are present lower one is counted as a pair)
-					if (pairCounter >= 1) {
-						pairValue = cards.get(i).getRank().getNumeral();
-						break outerloop;
+
+		if(tripsValue != 0){
+			//Find highest remaining pair
+			outerloop : for(int i = tempCards.size()-1; i >= 1; i--) {
+				pairCounter = 0;
+				for (int a = i - 1; a >= 0; a--) {
+					if (tempCards.get(i).getRank().equals(tempCards.get(a).getRank())) {
+						pairCounter++;
+						//Must be greater or equal to one (if two trips are present lower one is counted as a pair)
+						if (pairCounter >= 1) {
+							pairValue = tempCards.get(i).getRank().getNumeral();
+							break outerloop;
+						}
+					} else {
+						break;
 					}
-				} else {
-					break;
 				}
 			}
 		}
+
 		int [] retVal = new int [5];
 		if(tripsValue != 0 && pairValue != 0){
 			retVal[0] = FULLHOUSE;
@@ -152,17 +170,20 @@ public class HandEvaluator {
 	}
 
 	public static int[] hasFlush(ArrayList <Card> cards) {
+		//Store temporary arraylist of cards
+		ArrayList<Card> tempCards = new ArrayList<Card>(cards);
+
 		int flushCounter = 0;
 		int flushValue = 0;
 		//Same method as fullhouse, starts from end (highest flush)
 		//Requires sort by suit
-		outerloop : for (int i = cards.size() - 1; i >= 3; i--) {
+		outerloop : for (int i = tempCards.size() - 1; i >= 4; i--) {
 			flushCounter = 0;
 			for (int a = i - 1; a >= 0; a--) {
-				if (cards.get(i).getSuit().equals(cards.get(a).getSuit())) {
+				if (tempCards.get(i).getSuit().equals(tempCards.get(a).getSuit())) {
 					flushCounter++;
 					if (flushCounter == 4) {
-						flushValue = cards.get(i).getRank().getNumeral();
+						flushValue = tempCards.get(i).getRank().getNumeral();
 						break outerloop;
 					}
 				} else {
@@ -207,11 +228,12 @@ public class HandEvaluator {
 	public static int[] determineStrength(ArrayList<Card> cards) {
 
 		if (hasFourOfAKind(cards)[0] == QUAD) {
+			System.out.println("a");
 			return hasFourOfAKind(cards);
 		} else if (hasFullHouse(cards)[0] == FULLHOUSE){
 			return hasFullHouse(cards);
 		} else if (hasFlush(cards)[0] == FLUSH){
-		return hasFlush(cards);
+			return hasFlush(cards);
 		}
 		else{
 			int[] retVal = new int [5];
